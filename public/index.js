79d107bea99dd5c40de182be6c97c33daeb9d84f
James Pike <jpike@chilon.net>
2020-05-02 16:05:46 +0800

    have client reconnect websocket on close and improve dev experience

--- public/index.js
+++ public/index.js
@@ -1,27 +1,51 @@
 const start = () => {
-  const ws = new WebSocket("ws://" + window.location.host)
+  const ws = new WebSocket('ws://' + window.location.host)
 
-  const showsList = document.querySelector('#shows')
+  let ignore = false
+  const showList = document.querySelector('#shows')
+
+  const timeoutAttempt = setTimeout(() => {
+    if (!ignore) {
+      console.log('took too long to open websocket, trying again')
+      ignore = true
+      ws.close()
+      start()
+    }
+  }, 1000)
 
   ws.onopen = () => {
-    console.log('got websocket')
+    if (ignore) {
+      console.log('ignoring duplicate websocket connection')
+      ws.close()
+    } else {
+      clearTimeout(timeoutAttempt)
+      console.log('got websocket')
+    }
+  }
+
+  ws.onclose = () => {
+    if (!ignore) {
+      console.log('websocket closed, trying again')
+      setTimeout(start, 1000)
+      ignore = true
+    }
   }
 
   ws.onmessage = (message) => {
     const data = JSON.parse(message.data)
+    showList.innerHTML = ''
     if (data.type === 'shows') {
-      data.list.forEach(show => {
+      data.list.forEach((show) => {
         const li = document.createElement('li')
         li.textContent = show
-        showsList.appendChild(li)
+        showList.appendChild(li)
       })
     }
   }
 
-  showsList.onclick = ({ target }) => {
+  showList.onclick = ({ target }) => {
     ws.send(target.textContent)
   }
 }
 
 window.onload = start
-
--- src/index.ts
+++ src/index.ts
@@ -5,16 +5,21 @@ import { join as pathJoin } from 'path'
 import { readdir } from 'fs-extra'
 import { spawn, ChildProcess } from 'child_process'
 
+const port = 4921
 const showsDir = `${process.env.HOME}/shows`
 
 let running: ChildProcess | undefined
 
 async function sendShows(ctxt: Context) {
   const shows = await readdir(showsDir)
-  ctxt.websocket.send(JSON.stringify({
-    type: 'shows',
-    list: shows.filter(show => ! show.startsWith('.') && ! show.startsWith('doit'))
-  }))
+  ctxt.websocket.send(
+    JSON.stringify({
+      type: 'shows',
+      list: shows.filter(
+        (show) => !show.startsWith('.') && !show.startsWith('doit'),
+      ),
+    }),
+  )
 }
 
 async function listenToSocket(ctxt: Context) {
@@ -25,8 +30,8 @@ async function listenToSocket(ctxt: Context) {
       running.kill()
     } else {
       running = spawn('babies', ['n', show], {
-        stdio: 'inherit'
-      });
+        stdio: 'inherit',
+      })
 
       running.on('exit', () => {
         console.log('show finished')
@@ -39,12 +44,14 @@ async function listenToSocket(ctxt: Context) {
 }
 
 async function main(): Promise<void> {
-  const app = websockify(new App());
-  app.use(koaStatic(pathJoin(__dirname, '..', '/public')));
-  app.listen(4921);
+  const app = websockify(new App())
+  app.use(koaStatic(pathJoin(__dirname, '..', '/public')))
+  app.listen(port, () => {
+    console.log(`server listening on http://localhost:${port}`)
+  })
 
   app.ws.use((ctxt, next) => {
-    console.log('got websocket')
+    console.debug('got websocket')
     sendShows(ctxt)
     listenToSocket(ctxt)
     return next()
